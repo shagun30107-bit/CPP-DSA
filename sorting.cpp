2e90bbef39030fdefbad9f632c61cc1cbbdac950
shagun30107 <shagun30107@gmail.com>
2026-07-03 19:27:40 +0530

    merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -39,14 +39,33 @@ void insertionsort(int arr[], int n){
     }
 }
 
-void merge(vector<int> &arr, int low, int mid, int high)[
-    vector<int> temp;
-    int left = low;
-    int right = mid+1;
+void merge(vector<int>& arr, int low, int mid, int high) {
+        // Create temp arrays
+        vector<int> temp;
+        int left = low, right = mid + 1;
+
+        // Merge two sorted halves
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+            else
+                temp.push_back(arr[right++]);
+        }
+
+        // Copy remaining elements from left half
+        while (left <= mid)
+            temp.push_back(arr[left++]);
+
+        // Copy remaining elements from right half
+        while (right <= high)
+            temp.push_back(arr[right++]);
+
+        // Copy sorted elements back to original array
+        for (int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+    }
     
 
-
-]
 void ms(vector<int> &arr , int low, int high){
     if(low == high)return;
     int mid=(low+high) / 2;
--- tempCodeRunnerFile.cpp
+++
@@ -1,1 +0,0 @@
-cout << s_largest(arr,5)  ;
