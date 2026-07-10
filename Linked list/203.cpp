5d6c06cace65a418292ff4bb072a56928affe1c5
shagun30107 <shagun30107@gmail.com>
2026-07-10 15:22:47 +0530

    remove LL elements

---
+++ Linked list/203.cpp
@@ -0,0 +1,30 @@
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val == val){
+            ListNode* temp = head;
+            head = head->next ;
+            delete temp;
+        }
+        ListNode* temp = head;
+        while(temp && temp->next){
+            if(temp->next->val == val){
+                ListNode* del = temp->next;
+                temp->next= temp->next->next;
+                delete del;
+            }
+            else{
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
