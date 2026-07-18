4487b0eefb6230f508eaea549fb018b1ec672bb8
shagun30107 <shagun30107@gmail.com>
2026-07-18 20:57:54 +0530

    Palindrome of linked list

--- Linked list/Single LL/234.cpp
+++ Linked list/Single LL/234.cpp
@@ -29,4 +29,40 @@ public:
         }
         return true;   
     }
+
+// METHOD-2
+// TC-O(2*N) & SC-O(1)
+    ListNode* reverse(ListNode* head){
+        if(head==NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev=temp;
+            temp = front;
+        }
+        return prev;
+    }
+
+    ListNode* mid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* midH = reverse( mid(head));
+        while(midH != NULL){
+            if(temp->val != midH->val) return false;
+            temp=temp->next;
+            midH=midH->next;
+        }
+        return true;
+    }
+
 };
