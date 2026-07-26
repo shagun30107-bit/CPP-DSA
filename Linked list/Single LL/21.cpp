2837b1a3a62b3b8fed28c429ba97255d4b37c107
shagun30107 <shagun30107@gmail.com>
2026-07-26 12:05:53 +0530

    Merge two sorted List

--- Linked list/Single LL/21.cpp
+++ Linked list/Single LL/21.cpp
@@ -8,7 +8,7 @@ struct ListNode {
      ListNode(int x) : val(x), next(nullptr) {}
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
- 
+//brute solution
 class Solution {
 public:
     ListNode* convertArr2LL(vector<int> &arr){
@@ -41,3 +41,26 @@ public:
         return head;
     }
 };
+//Optimal solution
+    ListNode* mergeTwoLists2(ListNode* list1, ListNode* list2) {
+        //TC-O(N1+N2)--TWO POINTERS APPROACH
+        ListNode* temp1= list1;
+        ListNode* temp2= list2;
+        ListNode* dNode = new ListNode (-1);
+        ListNode* temp = dNode;
+        while(temp1 != NULL && temp2 != NULL){
+            if(temp1->val < temp2->val) {
+            temp->next = temp1;
+            temp=temp1;
+            temp1 = temp1->next;
+            }
+            else{
+            temp->next = temp2;
+            temp=temp2;
+            temp2=temp2->next;
+            }
+        }
+        if(temp1 != NULL) temp->next=temp1;
+        else temp->next=temp2;
+        return dNode->next;
+    }
