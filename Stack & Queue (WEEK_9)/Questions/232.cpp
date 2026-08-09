44e02a03faa982beaa82db1e545a56e599e7d535
shagun30107 <shagun30107@gmail.com>
2026-08-09 23:41:37 +0530

    Implement queue using stacks

---
+++ Stack & Queue (WEEK_9)/Questions/232.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+private:
+    stack <int> st1, st2;
+public:
+    MyQueue() {}
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        if(st2.empty()) return -1;
+        int n=st2.top();
+        st2.pop();
+        return n;
+    }
+    
+    int peek() {
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+
+        if(st2.empty()) return -1;
+        return st2.top();
+        
+    }
+    
+    bool empty() {
+        return st1.empty() && st2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
